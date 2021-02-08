fc5466460cf7242038074522ef4be5886488137e
rommac <rommac100@gmail.com>
2021-02-07 17:09:08 -0700

    updated the connect_vpn to use new get_region script

--- connect_vpn.sh
+++ connect_vpn.sh
@@ -1,8 +1,5 @@
- GNU nano 3.2                                                                        connect_vpn.sh                                                                        Modified
-
 #!/bin/bash
 # takens a specified token and starts grabbing stuff.
-cd /home/game/.scripts/manual-connections
 
 if [ $1 = "-k" ]; then
         echo Killing VPN
@@ -12,5 +9,5 @@ else
         export PIA_PF=true
         export PIA_AUTOCONNECT=openvpn_udp_standard
         export MAX_LATENCY=.07
-        /bin/bash /home/game/.scripts/manual-connections/get_region_and_token.sh
+         ./get_region.sh
 fi
